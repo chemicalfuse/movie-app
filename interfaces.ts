d35c7f094b0a89dde96fe1901d81a736d98f81b2
Anand Keshri <anandkeshri271@gmail.com>
2025-03-08 12:20:48 +0400

    Initial commit

---
+++ interfaces.ts
@@ -0,0 +1,12 @@
+export interface IMovie {
+  logoSrc: string;
+  title: string;
+  rating: string;
+  ageRating: string;
+  duration: string;
+  description: string;
+  starring: string;
+  genres: string;
+  tags: string;
+  trailerSrc: string;
+}
